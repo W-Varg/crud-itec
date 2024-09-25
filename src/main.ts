2cf6c0172185b12c8bc4cf70f5963f86a67e427f
W-Varg <kryshot05@gmail.com>
2024-09-24 21:26:18 -0400

    se agrego un console.log en main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { blue } from 'chalk';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+  await app
+    .listen(3001)
+    .then(() => console.info(blue.bold('Server running on port 3001')));
 }
 bootstrap();
