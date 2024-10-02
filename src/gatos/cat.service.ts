54954e054aa378578a925e4689aee21928c780d3
W-Varg <kryshot05@gmail.com>
2024-10-01 20:57:47 -0400

    se agrego metodo de eliminar, y actualizar

--- src/gatos/cat.service.ts
+++ src/gatos/cat.service.ts
@@ -4,7 +4,7 @@ import { CatDatosEntrada } from './datos-entrada.input';
 @Injectable()
 export class CatService {
   // crud
-  listaDeGatos = [];
+  listaDeGatos: CatDatosEntrada[] = []; // [{nombre:'garfield'}, {nombre:'pelusa'}, {nombre:'tom'}]
 
   create(body: CatDatosEntrada) {
     this.listaDeGatos.push(body);
@@ -15,11 +15,54 @@ export class CatService {
     return this.listaDeGatos;
   }
 
-  update() {
-    return `el gato fue actualizado`;
+  actualizar(nombreGato: string) {
+    // 'peludo'
+    const gatoEncontrado = this.listaDeGatos.find(
+      (elementoGato: CatDatosEntrada) => elementoGato.nombre === nombreGato,
+    ); // equivanlente a for
+    if (gatoEncontrado == null) {
+      return `el gato ${nombreGato} no se encontro en la lista`;
+    } else {
+      const posiciondeGato = this.listaDeGatos.findIndex(
+        (elementoGato) => elementoGato.nombre === nombreGato,
+      ); // equivanlente a for
+
+      this.listaDeGatos[posiciondeGato] = {
+        nombre: nombreGato,
+        raza: 'peludo',
+        edad: 2,
+        esBebe: false,
+      };
+
+      return {
+        gatos: this.listaDeGatos,
+        mensaje: `el gato ${nombreGato} fue actualizado`,
+      };
+    }
   }
 
-  delete() {
-    return `el gato fue eliminado`;
+  eliminar(nombreGato: string) {
+    const gatoEncontrado = this.listaDeGatos.find(
+      (elementoGato) => elementoGato.nombre === nombreGato,
+    );
+
+    if (gatoEncontrado === null) {
+      return `el gato ${nombreGato} no se encontro en la lista`;
+    } else {
+      // descarta elementos que no cumplen la condicion
+      this.listaDeGatos = this.listaDeGatos.filter((elementoGato) => {
+        const esIgual = elementoGato.nombre === nombreGato; // se encontro al gato
+        if (esIgual) {
+          // este gato se tiene que descartar de la lista
+        } else {
+          return elementoGato;
+        }
+      });
+
+      return {
+        gatos: this.listaDeGatos,
+        mensaje: `el gato ${nombreGato} fue eliminado`,
+      };
+    }
   }
 }
