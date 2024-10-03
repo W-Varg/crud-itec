f9e50bd52c7478f902ed6a82cd2f1de383a7b1a6
jonatan777s <jonatan777s@gmail.com>
2024-10-03 10:38:52 -0400

    modificaciones mascotas

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,11 @@ import { CatModule } from './gatos/cat.module';
 import { AvionModule } from './avion/avion.module';
 import { motoModule } from './moto/moto.module';
 import { AutosModule } from './autos/autos.module';
+import { MascotasModule } from './mascotas/mascotas.module';
 
 @Module({
-  imports: [CatModule, AvionModule, PaisModule, motoModule, AutosModule],
+  imports: [CatModule, AvionModule, PaisModule, motoModule, AutosModule, MascotasModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
