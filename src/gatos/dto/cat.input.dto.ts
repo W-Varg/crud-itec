460bb5e7e411e5d67385e68c9efd08b51fd1d2b9
W-Varg <kryshot05@gmail.com>
2024-10-09 20:27:02 -0400

    se agrego decoradores a mi CatDatosEntrada

--- src/gatos/dto/cat.input.dto.ts
+++ src/gatos/dto/cat.input.dto.ts
@@ -1,17 +1,27 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsBoolean, IsNotEmpty, IsNumber } from 'class-validator';
+import {
+  Contains,
+  IsBoolean,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  Length,
+} from 'class-validator';
 
 //dto de entrada
 export class CatDatosEntrada {
   @IsNotEmpty({ message: 'debe enviar el nombre del gato, es obligatorio' })
+  @Length(3, 10, { message: 'el nombre debe ser entre 3 y 10 caracteres' })
   @ApiProperty()
   nombre: string;
 
+  @Contains('si', { message: 'la raza debe contener la letra "si"' })
   @ApiProperty()
   raza: string;
 
   @IsNumber()
-  @ApiProperty({ default: 1 })
+  @IsOptional()
+  @ApiProperty({ default: 1, required: false })
   edad?: number;
 
   @IsBoolean({ message: 'campo esBebe debe ser true o false' }) // true o false , verdadero o falso
