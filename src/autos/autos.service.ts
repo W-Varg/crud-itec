a3e9dea0c8c2b084375693df89bbfe72ea031d5b
wilberli <tu_correo@example.com>
2024-10-10 21:40:06 -0400

    cambios

--- src/autos/autos.service.ts
+++ src/autos/autos.service.ts
@@ -1,16 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import { AutosDatosEntrada } from './datos-entrada.imputs';
+import { AutosModel } from './dto/autos.model';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class AutosService {
+  constructor(
+    @InjectModel(AutosModel.name) private autosconexion: Model<AutosModel>,
+  ) {}
   // crud
   listaDeAutos = [];
   create(body: AutosDatosEntrada) {
-    this.listaDeAutos.push(body);
+    //this.listaDeAutos.push(body);
+    this.autosconexion
+      .insertMany([
+        { modelo: 'jimmy', marca: 'Nissan', anio: 2007 },
+        { modelo: 'patrol', marca: 'Nissan', anio: 2010 },
+        { modelo: 'condor', marca: 'Nissan', anio: 2007 },
+      ])
+      .then(() => {
+        console.log('insertado');
+      });
+
+    const createdAutos = new this.autosconexion(body);
+    return createdAutos.save();
+
     return `Auto ${body.Marca} se creo exitosamente`;
   }
   listar() {
-    return this.listaDeAutos;
+    return this.autosconexion.find();
   }
   read() {
     return `Se encontro al Auto`;
