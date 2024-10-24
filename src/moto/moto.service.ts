9b686399251c638865b261e2e512fef740cae3b9
Cesar <cesario46@outlook.com>
2024-10-23 22:32:41 -0400

    prueba de variable de entorno

--- src/moto/moto.service.ts
+++ src/moto/moto.service.ts
@@ -3,14 +3,17 @@ import { motoEntrada } from './dto/data-input';
 import { MotoModel } from './dto/moto.model';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class motoService {
   constructor(
     @InjectModel(MotoModel.name) private motoCollection: Model<MotoModel>,
+    private readonly configservice: ConfigService,
   ) {}
   listaMotos = [];
   crear(body: motoEntrada) {
     this.listaMotos.push(body);
+    console.log(this.configservice.get('Variable_Motos'));
     return `moto ${body.marca} creada exitosamente`;
   }
 
